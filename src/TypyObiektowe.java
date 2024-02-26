6af017bfb11742e412739c0d4b7fd60aaeb3e409
Piotr Grzela <pagers87@gmail.com>
2024-02-27 00:27:53 +0100

    Typy Obiektowe

---
+++ src/TypyObiektowe.java
@@ -0,0 +1,23 @@
+public class TypyObiektowe {
+    public static void main(String[] args) {
+        //liczby stałoprzecinkowe
+        byte firstNumber = 127;
+        short secodNumber = 32689;
+        int thirdNumber = 32768999;
+        long fourthNumber = 214800L;
+
+        float fifthNumber = 4.99934F;
+        double sixthNumber = 3.99999999999999D;
+
+        boolean prawda = true;
+        boolean falsz = false;
+
+        char letter = 'A';
+
+        String hello = "Hello Peter";
+        //wskazuję index 0 czyli która litera się pojawi - w tej systuacji będzie to H
+        System.out.println(hello.charAt(0));
+
+    }
+
+}
---
+++ src/TypyProste.java
@@ -0,0 +1,16 @@
+public class TypyProste {
+
+    //liczby stałoprzecinkowe
+   byte firstNumber = 127;
+   short secodNumber = 32689;
+   int thirdNumber = 32768999;
+   long fourthNumber = 214800L;
+
+   float fifthNumber = 4.99934F;
+   double sixthNumber = 3.99999999999999D;
+
+   boolean prawda = true;
+   boolean falsz = false;
+
+   char letter = 'A';
+}
