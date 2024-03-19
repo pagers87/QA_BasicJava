fabcf8949221acff46ca9057742d996c5816ab09
Piotr Grzela <pagers87@gmail.com>
2024-03-19 23:59:23 +0100

    zmienne

---
+++ src/Zmienne.java
@@ -0,0 +1,17 @@
+//zarezerwowane miejsce w pamieci z określonym typem danych
+public class Zmienne {
+    String name = "Piotr";
+
+    public static void main(String[] args) {
+        int liczba1, liczba2, suma;
+
+        String name = "Piotr";
+        liczba1 = 1;
+        liczba2 = 2;
+        suma = liczba1 + liczba2;
+
+        name = "Kasia";
+        System.out.println(name);
+        System.out.println(suma);
+    }
+}
