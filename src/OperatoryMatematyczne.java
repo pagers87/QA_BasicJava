284de38aaaadbacfcf499ecfc9fbd7e04852f667
Piotr Grzela <pagers87@gmail.com>
2024-07-25 01:14:13 +0200

    OperatoryMatematyczne poprawki

--- src/OperatoryMatematyczne.java
+++ src/OperatoryMatematyczne.java
@@ -3,6 +3,21 @@ public class OperatoryMatematyczne {
     public static void main(String[] args) {
         int firstNumber = 4;
         int secondNumber = 6;
+        float thirdNumber = 4.0F;
+
+        int addition = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        float division = thirdNumber/secondNumber;
+        int mod = secondNumber%firstNumber;
+
+        System.out.println("Dodawanie " + addition);
+        System.out.println("Odejmowanie " + subtraction);
+        System.out.println("Mnożenie " + multiplication);
+        System.out.println("Dzielenie " + division);
+        System.out.println("Modulo " + mod);
+
+
 
 
     }
