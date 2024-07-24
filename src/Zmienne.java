22610058b92ae823d90ed96abdb874ad4ad8902a
Piotr Grzela <pagers87@gmail.com>
2024-07-25 00:51:37 +0200

    zmienne poprawa

--- src/Zmienne.java
+++ src/Zmienne.java
@@ -3,15 +3,17 @@ public class Zmienne {
     String name = "Piotr";
 
     public static void main(String[] args) {
-        int liczba1, liczba2, suma;
+        int liczba1;
+        int liczba2;
+        int result;
 
         String name = "Piotr";
         liczba1 = 1;
         liczba2 = 2;
-        suma = liczba1 + liczba2;
+        result = liczba1 + liczba2;
 
         name = "Kasia";
         System.out.println(name);
-        System.out.println(suma);
+        System.out.println(result);
     }
 }
